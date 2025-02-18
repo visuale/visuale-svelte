2b8961f8e3324a7b01ccc60a61428055ebba6816
James P <26826+visuale@users.noreply.github.com>
2025-02-17 19:11:39 -0600

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import '../app.css';
+	let { children } = $props();
+</script>
+<nav>
+	<a href="/">Home</a>
+	<a href="/projects">Projects</a>
+	<a href="/about">About</a>
+	<a href="/contact">Contact</a>
+</nav>
+
+{@render children()}
